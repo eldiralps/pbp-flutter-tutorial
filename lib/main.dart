daaada6138e5389e5d0b4efd6eff7455254f414a
eldiralps <eldiralps@gmail.com>
2022-11-17 15:50:06 +0700

    done tutorial terakhir yeayy

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:tutorial6/form.dart';
+import 'package:tutorial6/page/to_do_page.dart';
+import 'package:tutorial6/page/form.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -74,6 +76,16 @@ class _MyHomePageState extends State<MyHomePage> {
                 Navigator.pushReplacement(
                   context,
                   MaterialPageRoute(builder: (context) => const MyFormPage()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('To Do'),
+              onTap: () {
+                // Route menu ke halaman to do
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ToDoPage()),
                 );
               },
             ),
